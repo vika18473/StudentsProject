e5ef1fc691bd3fb956db515870420e642c1c72b4
vika18473 <vika18473@gmail.com>
2022-12-06 16:10:55 +0200

    Factory + Facade

--- index.ts
+++ index.ts
@@ -1,15 +1,16 @@
-import {Magician} from "./characters/magician"
-import { Thief } from "./characters/thief"
-import { Warrior } from "./characters/warrior"
-import {Characters} from "./characters/All"
+import {Create} from "./characters/Chair"
 
 
-let pers1 = new Magician()
-let pers2 = new Thief()
-let pers3 = new Warrior()
+let pers1 = Create.createPers("Magician")
+let pers2 = Create.createPers("Warrior")
+let pers3 = Create.createPers("Thief")
+let pers4 = Create.createPers("NO")
 
-console.log(pers1.hit())
-console.log(pers2.ability())
+
+console.log(pers1)
+console.log(pers2)
+console.log(pers3)
+console.log(pers4)
 
 
 
