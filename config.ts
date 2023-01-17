ccab25016d332fce3a4b5cf797357d9eff1c1723
vika18473 <vika18473@gmail.com>
2023-01-17 14:35:01 +0200

    add migration

--- config.ts
+++ config.ts
@@ -1,1 +1,9 @@
 export const jwtSecret = "secret123"
+export class PostgreSQL{
+    static username: string = "postgres"
+    static password: string = "dbrecz18473"
+    static host: string = "localhost"
+    static port: number = 5432
+    static database:string = "StudentsProject"
+    static dialect: string = "postgres"
+}
